3116f56141b853b0b5151f823f6a059b6f80e977
Mieszko Kłusek <mieszko.klusek@gmail.com>
2018-05-07 20:59:27 +0200

    Add characters to scene from script & create teams
        * Team now changes every 10s - temporary
        * Player can be changed by tab press

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+	void Start ()
+    {
+        player = GameManager.instance.curentCharacter;
+        offset = transform.position - player.transform.position;
+	}
+		
+	void LateUpdate ()
+    {
+        player = GameManager.instance.curentCharacter;
+        transform.position = player.transform.position + offset;
+	}
+    
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets._2D;
@@ -6,12 +7,23 @@ using UnityStandardAssets._2D;
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
-    public GameObject[] characters;
-    GameObject curentCharacter;
-    int characterIdx;
+    public GameObject curentCharacter;
 
-	// Use this for initialization
-	void Awake ()
+    private Dictionary<Team, List<GameObject>> teams;
+    private Dictionary<Team, Color> teamColors;
+    private Team activeTeam;
+    private Dictionary<Team, int> characterIdxs;
+
+    private Timer timer;
+
+    private enum Team
+    {
+        ALPHA,
+        BETA
+    };
+
+    // Use this for initialization
+    void Awake ()
     {
         if (instance == null)
             instance = this;
@@ -20,25 +32,63 @@ public class GameManager : MonoBehaviour {
 
         DontDestroyOnLoad(gameObject);
 
-        characterIdx = 0;
-        curentCharacter = characters[characterIdx];
-        enableCharacter(characterIdx);
+        teamColors = new Dictionary<Team, Color>()
+        {
+            {Team.ALPHA, new Color(1,0,0)},
+            {Team.BETA, new Color(0,0,1)}
+        };
+        createTeams(2);
+        activeTeam = Team.ALPHA;
+        enableCharacter(activeTeam, characterIdxs[activeTeam]);
+
+        this.InvokeRepeating("OnRoundFinished", 10, 10);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void createTeams(int players)
+    {
+        teams = new Dictionary<Team, List<GameObject>>();
+        characterIdxs = new Dictionary<Team, int>();
+
+        foreach (Team team in System.Enum.GetValues(typeof(Team)))
+        {
+            var teamCharacters = new List<GameObject>();
+            for (int i = 0; i < players; ++i)
+            {
+                GameObject character = (GameObject)Instantiate(Resources.Load("CharacterRobotBoy"));
+                var characterRenderer = character.GetComponent<Renderer>();
+                characterRenderer.material.color = teamColors[team];
+                teamCharacters.Add(character);
+            }
+            teams[team] = teamCharacters;
+            characterIdxs[team] = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            characterIdx = (characterIdx + 1) % characters.Length;
+            characterIdxs[activeTeam] = (characterIdxs[activeTeam] + 1) % teams[activeTeam].Count;
+            enableCharacter(activeTeam, characterIdxs[activeTeam]);
         }
-        enableCharacter(characterIdx);
-	}
+    }
 
-    private void enableCharacter(int idx)
+    private void enableCharacter(Team team, int idx)
     {
-        for(int i=0; i<characters.Length; i++)
+        curentCharacter = teams[team][idx];
+        foreach (Team t in System.Enum.GetValues(typeof(Team)))
         {
-            characters[i].GetComponent<Platformer2DUserControl>().enabled = (i == idx);
+            for(int i=0; i<teams[t].Count; i++)
+            {
+                bool current = (i == idx) && (t == team);
+                teams[t][i].GetComponent<Platformer2DUserControl>().enabled = current;
+            }
         }
+    }
+
+    private void OnRoundFinished()
+    {
+        activeTeam = activeTeam.Next();
+        enableCharacter(activeTeam, characterIdxs[activeTeam]);
     }
 }
